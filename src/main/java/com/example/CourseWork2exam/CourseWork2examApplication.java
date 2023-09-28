09bde682ba16333e188b8c513d95fd5d26ea7bd6
Alexlikhanov2011 <Likhanov-2011@yandex.ru>
2023-09-28 12:54:24 +0300

    начало курсовой

---
+++ src/main/java/com/example/CourseWork2exam/CourseWork2examApplication.java
@@ -0,0 +1,13 @@
+package com.example.CourseWork2exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWork2examApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseWork2examApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CourseWork2exam/CourseWork2examApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CourseWork2exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWork2examApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
